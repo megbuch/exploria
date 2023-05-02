aa69a2496272bd3d2285fbece242389eb55ca6b0
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-05-01 22:29:17 -0700

    styles parks index page

--- src/pages/ParksIndexPage/ParksIndexPage.jsx
+++ src/pages/ParksIndexPage/ParksIndexPage.jsx
@@ -6,37 +6,49 @@ export default function ParksIndexPage() {
   const [parksData, setParksData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
 
-  const parksPerPage = 50;
+  const parksPerPage = 16;
 
   const currentParks = parksData.slice(
     currentPage * parksPerPage,
     (currentPage + 1) * parksPerPage
   );
 
+  const fetchParksData = async () => {
+    try {
+      const response = await axios.get(
+        "https://developer.nps.gov/api/v1/parks/",
+        {
+          headers: {
+            "X-Api-Key": import.meta.env.VITE_NPS_API_KEY,
+            "Content-Type": "application/json",
+          },
+          params: {
+            limit: parksPerPage,
+            start: currentPage * parksPerPage,
+          },
+        }
+      );
+
+      setParksData((prevParksData) => [
+        ...prevParksData,
+        ...response.data.data,
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-    axios
-      .get("https://developer.nps.gov/api/v1/parks/", {
-        headers: {
-          "X-Api-Key": import.meta.env.VITE_NPS_API_KEY,
-          "Content-Type": "application/json",
-        },
-        params: {
-          limit: parksPerPage,
-          start: currentPage * parksPerPage,
-        },
-      })
-      .then((response) => {
-        setParksData((prevParksData) => [
-          ...prevParksData,
-          ...response.data.data,
-        ]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    fetchParksData();
   }, [currentPage]);
 
-  const nextPage = () => {
+  const nextPage = async () => {
+    const nextPageStart = (currentPage + 1) * parksPerPage;
+
+    if (nextPageStart >= parksData.length) {
+      await fetchParksData();
+    }
+
     setCurrentPage(currentPage + 1);
   };
 
@@ -45,13 +57,9 @@ export default function ParksIndexPage() {
   };
 
   return (
-    <div>
-      <div>
-        {currentParks.map((park) => (
-          <div key={park.id}>{park.fullName}</div>
-        ))}
-      </div>
-      <div>
+    <section className="ParksIndexPage">
+      <h1>All Parks</h1>
+      <div className="pagination-buttons">
         <button onClick={lastPage} disabled={currentPage === 0}>
           Prev
         </button>
@@ -62,6 +70,20 @@ export default function ParksIndexPage() {
           Next
         </button>
       </div>
-    </div>
+      <div className="parks-list">
+        {currentParks.map((park) => (
+          <div
+            className="park-element"
+            key={park.id}
+            style={{
+              backgroundImage: `linear-gradient(transparent, #000000ca), url(${park.images[0].url})`,
+            }}
+          >
+            <p className="name">{park.fullName}</p>
+            <p className="state">{park.states.slice(0, 2)}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
