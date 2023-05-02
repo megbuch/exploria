742e47a5a358127604e9ca2d118ff9ac4c4bf748
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-05-01 19:07:54 -0700

    adds pagination to parks index page

--- src/pages/ParksIndexPage/ParksIndexPage.jsx
+++ src/pages/ParksIndexPage/ParksIndexPage.jsx
@@ -4,6 +4,14 @@ import axios from "axios";
 
 export default function ParksIndexPage() {
   const [parksData, setParksData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const parksPerPage = 50;
+
+  const currentParks = parksData.slice(
+    currentPage * parksPerPage,
+    (currentPage + 1) * parksPerPage
+  );
 
   useEffect(() => {
     axios
@@ -12,20 +20,48 @@ export default function ParksIndexPage() {
           "X-Api-Key": import.meta.env.VITE_NPS_API_KEY,
           "Content-Type": "application/json",
         },
+        params: {
+          limit: parksPerPage,
+          start: currentPage * parksPerPage,
+        },
       })
       .then((response) => {
-        setParksData(response.data.data);
+        setParksData((prevParksData) => [
+          ...prevParksData,
+          ...response.data.data,
+        ]);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [currentPage]);
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const lastPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
 
   return (
     <div>
-      {parksData.map((park) => (
-        <div key={park.id}>{park.fullName}</div>
-      ))}
+      <div>
+        {currentParks.map((park) => (
+          <div key={park.id}>{park.fullName}</div>
+        ))}
+      </div>
+      <div>
+        <button onClick={lastPage} disabled={currentPage === 0}>
+          Prev
+        </button>
+        <button
+          onClick={nextPage}
+          disabled={currentParks.length < parksPerPage}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
